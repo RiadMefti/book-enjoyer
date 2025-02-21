69ec6620398349ff9a453411a2a2ce34bb1671c3
Riad Mefti <104847379+RiadMefti@users.noreply.github.com>
2025-02-21 10:03:30 -0500

    feat: remove unused POST method from user API

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,16 +1,7 @@
 import { NextResponse } from "next/server";
 import { db } from "@/db";
-import { usersTable } from "@/db/schema";
+
 export async function GET() {
   const users = await db.query.user.findMany();
   return NextResponse.json(users);
-}
-
-export async function POST(req: Request) {
-  const user = await db.insert(usersTable).values({
-    age: 25,
-    email: "tester",
-    name: "tester",
-  });
-  return NextResponse.json(user);
 }
